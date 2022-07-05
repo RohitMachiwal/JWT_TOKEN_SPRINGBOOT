8aadb21c08c15c2280d9496e6efe59f218029a16
RohitMachiwal <rohitmachiwal@yahoo.com>
2022-07-05 16:49:30 +0530

    JWT TOKEN

---
+++ src/main/java/com/jwtdemo/jwt/demo/model/JwtRequest.java
@@ -0,0 +1,14 @@
+package com.jwtdemo.jwt.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtRequest {
+
+    private  String username;
+    private  String password;
+}
---
+++ src/main/java/com/jwtdemo/jwt/demo/model/JwtResponse.java
@@ -0,0 +1,13 @@
+package com.jwtdemo.jwt.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtResponse {
+
+    private  String jwtToken;
+}
